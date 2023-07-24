c84923b1443c17f234aeb7510d09c176f300edd9
nishantatras <nishantatras@gmail.com>
2023-07-24 23:17:04 +0530

    New Problem-Solutions added

--- Problems/SearchingProblems.java
+++ Problems/SearchingProblems.java
@@ -180,20 +180,71 @@ public class SearchingProblems {
         return l;
     }
 
-     public static int findPeakElement(int[] nums) {
-         
-    int si=0,ei=nums.length-1,mid=0;
-    
-        while(si<ei){
-        mid=(si+ei)/2;
-        if(nums[mid]>nums[mid+1]){
-        ei=mid;
+    public static int findPeakElement(int[] nums) {
+
+        int si = 0, ei = nums.length - 1, mid = 0;
+
+        while (si < ei) {
+            mid = (si + ei) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                ei = mid;
+            } else {
+                si = mid + 1;
+            }
         }
-        else{
-        si=mid+1;
-        }
-        }
-        return si; 
+        return si;
     }
 
+    // Searching In Rotated Sorted Array
+    public static int rotatedArraySearch(int[] nums, int target) {
+        int p = pivot(nums);
+        if (p == -1) {
+            return bs(nums, target, 0, nums.length - 1);
+        }
+        if (nums[p] == target) {
+            return p;
+        }
+        if (target >= nums[0]) {
+            return bs(nums, target, 0, p - 1);
+        } else {
+            return bs(nums, target, p + 1, nums.length - 1);
+        }
+    }
 
+    // Pivot Finding in Rotated Sorted Array
+    public static int pivot(int[] arr) {
+        int si = 0, ei = arr.length - 1;
+        while (si <= ei) {
+            int mid = (si + ei) / 2;
+            if (mid < ei && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (mid > si && arr[mid] < arr[mid - 1]) {
+                return mid - 1;
+            }
+
+            if (arr[si] >= arr[mid]) {
+                ei = mid - 1;
+            } else {
+                si = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    // BinarySearch Method
+    public static int bs(int[] arr, int target, int si, int ei) {
+        while (si <= ei) {
+            int mid = (si + ei) / 2;
+            if (arr[mid] > target) {
+                ei = mid - 1;
+            } else if (arr[mid] < target) {
+                si = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+}
